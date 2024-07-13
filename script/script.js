5f32129f8295f83a2ee31d9204c45978964e48a1
Eduardo Albuquerque Ribeiro <eduardoribeiro.advg@gmail.com>
2024-07-13 15:48:33 -0300

    Adicionadas as funções: Alterar tema no canto superior direito; Botão Contatos no footer, que ao ser clicado abrirá uma janela modal onde é possível copiar os links dos contatos; Alterações cosméticas: Alteração do site, tanto a cor de fundo, quanto a cor do header, footer e as linhas de divisão. Alteração na cor dos ícones ao alterar o tema.

--- script/script.js
+++ script/script.js
@@ -1,7 +1,47 @@
-function copyToClipboard(){
-    var copyText = document.getElementById(#contact-url);
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // Para dispositivos móveis
+document.addEventListener('DOMContentLoaded', () => {
+    verifyTheme();
+    
+})
+
+function verifyTheme(){
+    const theme = localStorage.getItem('theme');
+    if(theme){
+        document.body.setAttribute('data-theme', theme);
+    }
+}
+
+function changeTheme(){
+    const theme = document.body.getAttribute('data-theme');
+    console.log(theme);
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    document.body.setAttribute("data-theme", newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+function clickCard() {
+    var modal = document.getElementById("contactModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function copyToClipboard(elementId) {
+    var link = document.getElementById(elementId).textContent;
+    var tempInput = document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = link;
+    tempInput.select();
     document.execCommand("copy");
-    alert("Informação copiada: " + copyText.value);
+    document.body.removeChild(tempInput);
+    alert("Link copiado: " + link);
 }
